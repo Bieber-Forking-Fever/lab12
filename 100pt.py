0f2ebd5dbe8fe93cae75a5f4114ea1cb80a69994
Bieber-Forking-Fever <asfiohqwtgpowh@gmail.com>
2014-11-03 15:15:49 -0600

    ASrioawhrapwhepwoae

--- 100pt.py
+++ 100pt.py
@@ -25,13 +25,17 @@ class MyApp:
 		self.button1 = Button(self.myContainer1)
 		self.button1.configure(text="Left", background= "green")
 		self.button1.grid(row=0,column=0)
-		
-	        # Add a second button!
-				
+
+                self.button2 = Button(self.myContainer1)
+		self.button2.configure(text="Right", background= "green")
+		self.button2.grid(row=0,column=1)
+
+
 		
 						
 		# "Bind" an action to the first button												
 		self.button1.bind("<Button-1>", self.button1Click)
+                self.button2.bind("<Button-1>", self.button2Click)
 		# Create the code to bind an action to the second button
 		# Do not change "<Button-1>"
 		 
@@ -42,10 +46,15 @@ class MyApp:
 
 		
 	def button1Click(self, event):   
-		# Make the oval move to the left!
-                # "global" makes sure that we can access our oval and our drawpad
-		global oval
-		global drawpad
+            drawpad.move(oval,-5,0)
+	def button2Click(self, event): 
+	    drawpad.move(oval,5,0)
+
+
+
+
+global oval
+global drawpad
 	
 	# Add the event handler for the second button to make it move right!
 	
